41526c31371568d1be2e20bfbe21770e75a65f15
vitorgomes <santosgomesv@gmail.com>
2023-01-03 12:52:48 -0300

    erro ao cadastradar formulario

--- Unidades/views.py
+++ Unidades/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import redirect, render
 from Autenticacao.models import ORDEN
+from Unidades.models import CLIENTE_EXAME
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     OS = ORDEN.objects.all()
     return render(request,'home.html',{'OS':OS})
 
+
+
+@login_required(login_url='logar')
 def cadastro_cliente(request):
+
     if request.method == "GET":
+        print(request.method)
         return render(request, 'cadastro_cliente.html')
+        
     elif request.method == "POST":
+        exames = CLIENTE_EXAME.objects.all()
         NOME = request.POST.get('NOME')
         LOGRADOURO = request.POST.get('LOGRADOURO')
         NUMERO = request.POST.get('NUMERO')
@@ -19,6 +28,10 @@ def cadastro_cliente(request):
         DATA_NASCIMENTO = request.POST.get('DATA_NASCIMENTO')
         EMAIL = request.POST.get('EMAIL')
         EXAME = request.POST.get('EXAME')
-        FOTO =  request.POST.get('FOTO')
-        
-        return render(request,'cadastro_cliente.html')
+        FOTO =  request.POST.get('FOTO')       
+       
+
+        ordem =ORDEN(NOME=NOME,LOGRADOURO=LOGRADOURO,NUMERO=NUMERO,BAIRRO=BAIRRO,CIDADE=CIDADE,TELEFONE=TELEFONE,CPF=CPF,DATA_NASCIMENTO=DATA_NASCIMENTO,EMAIL=EMAIL,EXAME=EXAME,FOTO=FOTO)
+        ordem.save()
+        print(request.method)
+        return render(request,'cadastro_cliente.html',{'exames':exames})
