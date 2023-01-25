8135ae77078ab728989ff2e9ee57ff7807e55d15
vitorgomes <santosgomesv@gmail.com>
2023-01-25 12:05:57 -0300

    js para o sub menu feito falta remover os dados

--- templates/static/Unidades/js/os.js
+++ templates/static/Unidades/js/os.js
@@ -1,13 +1,24 @@
 function update() {
-  var select = document.getElementById('servico');
-  var Sub_Serviço = document.getElementById('Sub_Serviço');
+  var select = document.getElementById('SERVICO');
+  const Sub_Serviço = document.getElementById('SUB_SERVICO');
+
 
   if (select.value =='Encomenda'){
-    alert('Encomenda');
+      const OptionsList = ["Ruby", "JavaScript", "Python", "GoLang"];
+      OptionsList.forEach((opt) => {
+      option = new Option(opt, opt.toLowerCase());
+      Sub_Serviço.options[Sub_Serviço.options.length] = option;
+    });
   }
 
-  if (select.value =='Pronta_entrega'){
-    alert('Pronta_entrega')
+  if (select.value =='Pronta entrega'){
+    
+    const OptionsList = ["c#", "teste", "test1", "22"];
+    OptionsList.forEach((opt) => {
+    option2 = new Option(opt, opt.toLowerCase());
+    Sub_Serviço.options[Sub_Serviço.options.length] = option2;
+    console.log(OptionsList.length=0);
+  });
   }
 }
 
