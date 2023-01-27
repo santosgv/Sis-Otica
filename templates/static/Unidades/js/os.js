f38c875757692fb965e1e338605d3d58377ff1c3
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2023-01-27 19:16:21 -0300

    Javascript corrigido com servico e sub

--- templates/static/Unidades/js/os.js
+++ templates/static/Unidades/js/os.js
@@ -1,38 +1,44 @@
-function update() {
-  var select = document.getElementById('SERVICO');
-  const Sub_Serviço = document.getElementById('SUB_SERVICO');
-  var OptionsList = []
+document.getElementById("SERVICO").addEventListener("change", atualizarSegundoSelect);
 
+function atualizarSegundoSelect() {
+  var primeiroSelect = document.getElementById("SERVICO");
+  var segundoSelect = document.getElementById("SUB_SERVICO");
+  // limpa todas as opções do segundo select
+  segundoSelect.innerHTML = "";
+  // obtém a seleção do primeiro select
+  var selecao = primeiroSelect.options[primeiroSelect.selectedIndex].value;
+  // cria novas opções para o segundo select de acordo com a seleção do primeiro select
+  if (selecao === "Encomenda") {
+      var opcao1 = document.createElement("option");
+      opcao1.value = "Óculos de Grau";
+      opcao1.text = "Óculos de Grau";
+      segundoSelect.appendChild(opcao1);
 
-  if (select.value =='Encomenda'){
-    Sub_Serviço.empty();
-    
-     OptionsList = ["Óculos de Grau", "Conserto", "lente de contato"];
-      OptionsList.forEach((opt) => {
-      option = new Option(opt, opt.toLowerCase());
-      Sub_Serviço.options[Sub_Serviço.options.length] = option;
-     
-    });
-  }
+      var opcao2 = document.createElement("option");
+      opcao2.value = "Conserto";
+      opcao2.text = "Conserto";
+      segundoSelect.appendChild(opcao2);
 
-  if (select.value =='Pronta Entrega'){
-    Sub_Serviço.empty();
-     OptionsList = ["óculos solar", "lente de contato", "outros"];
-      OptionsList.forEach((opt) => {
-      option = new Option(opt, opt.toLowerCase());
-      Sub_Serviço.options[Sub_Serviço.options.length] = option;  
-      
-    });
-  }
-}
+      var opcao3 = document.createElement("option");
+      opcao3.value = "lente de contato";
+      opcao3.text = "lente de contato";
+      segundoSelect.appendChild(opcao3);
+  } 
+  
+  else if (selecao === "Pronta Entrega") {
+      var opcao4 = document.createElement("option");
+      opcao4.value = "Oculos solar";
+      opcao4.text = "Oculos solar";
+      segundoSelect.appendChild(opcao4);
 
-update();
+      var opcao5 = document.createElement("option");
+      opcao5.value = "lente de contato";
+      opcao5.text = "lente de contato";
+      segundoSelect.appendChild(opcao5);
 
-function link(){
-  var select = document.getElementById('PAGAMENTO');
-
-  if(select.value=='Carne'){
-    var div = document.querySelector("div[class='alert alert-warning _carner']");
-    div.style.display='block';
+      var opcao5 = document.createElement("option");
+      opcao5.value = "Outros";
+      opcao5.text = "Outros";
+      segundoSelect.appendChild(opcao5);
   }
 }
