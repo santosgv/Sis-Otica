09abeb3771775dc8cc43b3ad0a3e8e3253b1a037
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2023-02-08 19:14:20 -0300

    pasta bakand

--- templates/static/Unidades/js/carne.js
+++
@@ -1,7 +0,0 @@
-function link(){
-    var select = document.getElementById('PAGAMENTO');
-    var div = document.getElementById('_carner');
-    if (select.value =='E'){
-        div.style.display = "block";
-    }
-}
