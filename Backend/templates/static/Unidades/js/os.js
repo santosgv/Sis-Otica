b9eb593db0aab7f18195eaa298168ed0d5366045
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2023-02-24 20:39:25 -0300

    validaçoes

--- Backend/templates/static/Unidades/js/os.js
+++ Backend/templates/static/Unidades/js/os.js
@@ -9,6 +9,11 @@ function atualizarSegundoSelect() {
   var selecao = primeiroSelect.options[primeiroSelect.selectedIndex].value;
   // cria novas opções para o segundo select de acordo com a seleção do primeiro select
   if (selecao === "Encomenda") {
+    var opcao0 = document.createElement("option");
+      opcao0.value = "Selecione um Sub Serviço";
+      opcao0.text = "Selecione um Sub Serviço";
+      segundoSelect.appendChild(opcao0);
+
       var opcao1 = document.createElement("option");
       opcao1.value = "Óculos de Grau";
       opcao1.text = "Óculos de Grau";
@@ -27,13 +32,13 @@ function atualizarSegundoSelect() {
   
   else if (selecao === "Pronta Entrega") {
       var opcao4 = document.createElement("option");
-      opcao4.value = "Oculos solar";
-      opcao4.text = "Oculos solar";
+      opcao4.value = "Oculos de Sol";
+      opcao4.text = "Oculos de Sol";
       segundoSelect.appendChild(opcao4);
 
       var opcao5 = document.createElement("option");
-      opcao5.value = "lente de contato";
-      opcao5.text = "lente de contato";
+      opcao5.value = "lente de contato pronta";
+      opcao5.text =  "lente de contato pronta";
       segundoSelect.appendChild(opcao5);
 
       var opcao5 = document.createElement("option");
--- Backend/templates/static/Unidades/js/os_Segundo.js
+++ Backend/templates/static/Unidades/js/os_Segundo.js
@@ -7,26 +7,57 @@ function atualizarSegundoSelect() {
     var _observacao = document.getElementById("_observacao");
     var valor = segundoSelect.options[segundoSelect.selectedIndex].value;
 
-    if (valor === 'lente de contato'){
-        _laboratorio.style.display ='none';
-        _receita.style.display ='none';
-        _armacao.style.display ='none';
-       
-    }else{
+
+    if (valor =="Óculos de Grau"){
+        _receita.style.display = 'block';
         _laboratorio.style.display ='block';
-        _receita.style.display ='block';  
-        _armacao.style.display ='block'; 
+        _lentes.style.display = 'block';
+        _armacao.style.display = 'block';
+        _observacao.style.display='block';
     }
 
-    if (valor ==='Conserto'){
+    else if (valor == "lente de contato"){
+        _receita.style.display ='block';
+        _lentes.style.display = 'block';
         _laboratorio.style.display ='none';
-        _receita.style.display ='none';
-        _armacao.style.display ='none';
-        _lentes.style.display ='none';
-    }else{
-        _laboratorio.style.display ='block';
-        _receita.style.display ='block';  
-        _armacao.style.display ='block'; 
-        _lentes.style.display ='block';
+        _armacao.style.display = 'none';
+        _observacao.style.display='none';
     }
+    else if (valor=="lente de contato pronta" || valor =="Oculos de Sol"){
+        _lentes.style.display = 'block';
+        _armacao.style.display = 'block';
+
+        _receita.style.display = 'none';
+        _laboratorio.style.display ='none';
+        _observacao.style.display='none';
+    }
+
+    else if (valor == "Outros"){
+        _observacao.style.display ='block';
+
+        _receita.style.display = 'none';
+        _laboratorio.style.display ='none';
+        _lentes.style.display = 'none';
+        _armacao.style.display = 'none';
+    }
+
+    else if (valor =="Conserto"){
+        _observacao.style.display ='block';
+        
+        _receita.style.display = 'none';
+        _laboratorio.style.display ='none';
+        _lentes.style.display = 'none';
+        _armacao.style.display = 'none';
+    }
+   
+    else
+    {
+        _receita.style.display = 'none';
+        _laboratorio.style.display ='none';
+        _lentes.style.display = 'none';
+        _armacao.style.display = 'none';
+        _observacao.style.display='none';
+    }
+    
+    
 }
