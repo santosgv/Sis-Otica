0bff1eaade2f1a0a3656542b2a795f2f71013541
vitor <santosgomesv@gmail.com>
2023-09-03 20:09:18 -0300

    banco main

--- Backend/Sis/settings.py
+++ Backend/Sis/settings.py
@@ -77,28 +77,11 @@ AUTH_USER_MODEL= "Autenticacao.USUARIO"
 
 DATABASES = {
     'default': {
-       'ENGINE': 'django_tenants.postgresql_backend',
-            'NAME': config('BANCO'),
-        'USER': config('BANCO_USER'),
-        'PASSWORD': config('BANCO_PASSWORD'),
-        'HOST': config('BANCO_HOST'),
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
-DATABASE_ROUTERS = (
-
-    'django_tenants.routers.TenantSyncRouter',
- )
-
-TENANT_LIMIT_SET_CALLS = True
-
-
-TENANT_MODEL = "Cliente.Cliente"
-
-TENANT_DOMAIN_MODEL = "Cliente.Domain"
-
-TENANT_COLOR_ADMIN_APPS = False
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
