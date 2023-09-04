7233b355c284760a3ad9f98d7c374a089a7b873e
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2023-09-04 15:41:19 -0300

    ajustes layout

--- Backend/Sis/settings.py
+++ Backend/Sis/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-     'django_tenants.middleware.main.TenantMainMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
