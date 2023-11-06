cc6e0ea36773356d4bd1d74f54a7bb9a93802d51
Vitor <santosgomesv@gmail.com>
2023-11-06 11:11:26 -0300

    correcao signals

--- Backend/Cliente/signals.py
+++ Backend/Cliente/signals.py
@@ -1,14 +1,13 @@
 from django.dispatch import receiver
 from django_tenants.signals import  post_schema_sync
-from django.db.models.signals import pre_save
 from django_tenants.models import TenantMixin
 from django.utils import timezone
-from .models import Cliente
+from .models import Cliente,Domain
 from django_tenants.utils import tenant_context
-from .Autenticacao.USUARIO import USUARIO
+from Autenticacao.models import USUARIO
 from django.core.mail import send_mail
 from decouple import config
-from .tasks import Envia_email_com_super_usuario,desativar_clientes
+
 
 @receiver(post_schema_sync, sender=TenantMixin)
 def created_user_client(sender, **kwargs):
@@ -21,5 +20,16 @@ def created_user_client(sender, **kwargs):
             FUNCAO='G'
         )
         super_user.save()
-        print('Foi chamado')
+        subject = 'Novo Cliente Criado'
+        message = f'''O Cliente {client.nome} foi criado com sucesso!
+        Nome do Banco : {client.schema_name}
+        Usuario Administrador : {client.schema_name}
+        Senha Administrador : {client.schema_name}
+        valido ate : {client.pago_ate}
+        Descricao : {client.descricao}
+        E-mail : {client.email}
+        '''
+        from_email = config('EMAIL_HOST_USER')
+        recipient_list = ['santosgomesv@gmail.com',client.email]
+        send_mail(subject, message, from_email, recipient_list, fail_silently=False)
         return 'Super Usuario Criado'
