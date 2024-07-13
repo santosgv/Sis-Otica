55c4f90676f5f8b966f688b2dcb0fe6f905e9240
vitor <santosgomesv@gmail.com>
2024-07-13 16:23:50 -0300

    removendo cache

--- Backend/Sis/settings.py
+++ Backend/Sis/settings.py
@@ -124,8 +124,8 @@ DATABASES = {
 
 SESSION_COOKIE_AGE = 86400 # 24 horas * 60 minutos * 60 segundos
 
-SESSION_ENGINE = 'django.contrib.sessions.backends.file'
-SESSION_FILE_PATH = os.path.join(BASE_DIR,'tmp/')
+#SESSION_ENGINE = 'django.contrib.sessions.backends.file'
+#SESSION_FILE_PATH = os.path.join(BASE_DIR,'tmp/')
 
 
 DATABASE_ROUTERS = (
