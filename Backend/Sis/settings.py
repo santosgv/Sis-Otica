ee71d8b61f701e60acf05dcfa0affc615369451f
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2024-10-01 14:04:01 -0300

    correçao impressao e relatorios

--- Backend/Sis/settings.py
+++ Backend/Sis/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_sonar',
+  #  'django_sonar',
     'compressor',
     'debug_toolbar',
  #   'elasticapm.contrib.django',
@@ -86,7 +86,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-      'django_sonar.middlewares.requests.RequestsMiddleware',
+   #   'django_sonar.middlewares.requests.RequestsMiddleware',
 ]
 
 ROOT_URLCONF = 'Sis.urls'
