db8fb7a19e9897ef8913ca70cda60b98981683e4
vitor <santosgomesv@gmail.com>
2024-10-01 00:10:44 -0300

    comentario no sonar

--- Backend/Sis/settings.py
+++ Backend/Sis/settings.py
@@ -86,7 +86,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-      'django_sonar.middlewares.requests.RequestsMiddleware',
+    #  'django_sonar.middlewares.requests.RequestsMiddleware',
 ]
 
 ROOT_URLCONF = 'Sis.urls'
