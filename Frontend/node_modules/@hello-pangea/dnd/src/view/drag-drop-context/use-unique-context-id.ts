7202da07771d89b5cf84ddbca54569d6d866c777
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2025-07-16 14:38:02 -0300

    Delete Frontend/node_modules directory

--- Frontend/node_modules/@hello-pangea/dnd/src/view/drag-drop-context/use-unique-context-id.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import type { ContextId } from '../../types';
-
-function useUniqueContextId(): ContextId {
-  return React.useId();
-}
-
-export default useUniqueContextId;
