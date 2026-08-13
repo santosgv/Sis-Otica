93dbfe56322e479aa94c2240865ec997c12d9c79
Vitor <santosgomesv@gmail.com>
2026-08-12 22:47:17 -0300

    remoçao do retelimit

--- Backend/integracoes/urls.py
+++ Backend/integracoes/urls.py
@@ -7,10 +7,10 @@ from django_ratelimit.decorators import ratelimit
 app_name = 'notificacoes'
 
 urlpatterns = [
-    path('whatsapp/',ratelimit(key='ip', method='GET', rate='10/m')                          (WhatsAppConfigView.as_view()),        name='whatsapp_config'),
-    path('whatsapp/qr/',ratelimit(key='ip', method='GET', rate='10/m')                       (WhatsAppQRView.as_view()),            name='whatsapp_qr'),
-    path('whatsapp/status/',ratelimit(key='ip', method='GET', rate='10/m')                   (WhatsAppStatusView.as_view()),        name='whatsapp_status'),
-    path('whatsapp/desconectar/',ratelimit(key='ip', method='GET', rate='10/m')              (WhatsAppDesconectarView.as_view()),   name='whatsapp_desconectar'),
-    path('whatsapp/teste/',ratelimit(key='ip', method='GET', rate='10/m')                    (WhatsAppTesteView.as_view()),         name='whatsapp_teste'),
-    path('os/<int:pk>/enviar/<str:tipo>/', ratelimit(key='ip', method='GET', rate='10/m')    (WhatsAppEnviarOsView.as_view()), name='enviar_os'),
+    path('whatsapp/',                        (WhatsAppConfigView.as_view()),        name='whatsapp_config'),
+    path('whatsapp/qr/',                     (WhatsAppQRView.as_view()),            name='whatsapp_qr'),
+    path('whatsapp/status/',                 (WhatsAppStatusView.as_view()),        name='whatsapp_status'),
+    path('whatsapp/desconectar/',            (WhatsAppDesconectarView.as_view()),   name='whatsapp_desconectar'),
+    path('whatsapp/teste/',                  (WhatsAppTesteView.as_view()),         name='whatsapp_teste'),
+    path('os/<int:pk>/enviar/<str:tipo>/',   (WhatsAppEnviarOsView.as_view()), name='enviar_os'),
 ]
